a8a96af3133f744eae16436eeb5a77b1594f39c6
Tjens23 <tjens23@student.sdu.dk>
2025-08-13 11:04:40 +0200

    breh

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}" 
-REPO="ghcr.io/$docker_username/"
-echo "${REPO}"
-docker build -t "${REPO}micronaut-app:latest" -t "${REPO}micronaut-app:1.0-$Tag" app/
+[[ -z "$GIT_COMMIT" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
+REPO="$1"
+IMAGE="ghcr.io/${REPO}/micronaut-app"
+echo "Building Docker image: $IMAGE"
+docker build -t "$IMAGE:latest" -t "$IMAGE:1.0-$Tag" app/
