7508cb921ba7b8a6c7fb75b7da5f14e150fb7485
Tjens23 <tjens23@student.sdu.dk>
2025-08-13 11:09:40 +0200

    breh

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -2,6 +2,8 @@
 set -e
 [[ -z "$GIT_COMMIT" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
 REPO="$1"
-IMAGE="ghcr.io/${REPO}/micronaut-app"
+# Convert repository name to lowercase for Docker tag
+REPO_LOWERCASE=$(echo "$REPO" | tr '[:upper:]' '[:lower:]')
+IMAGE="ghcr.io/${REPO_LOWERCASE}/micronaut-app"
 echo "Building Docker image: $IMAGE"
 docker build -t "$IMAGE:latest" -t "$IMAGE:1.0-$Tag" app/
